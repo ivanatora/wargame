ffa1d0f82fc410ad6c9e7c0235ff9594f034be21
Ivan <ivanatora@gmail.com>
2017-03-08 17:42:17 +0200

    begin homepage

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Wargame</title>
+
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.3/dist/leaflet.css" />
+
+    </head>
+    <body>
+
+
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+        <script src="https://unpkg.com/leaflet@1.0.3/dist/leaflet.js"></script>
+        <script src="/js/custom.js"></script>
+    </body>
+</html>
