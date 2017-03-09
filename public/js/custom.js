86768e5562074c68dfa8cbbb1aca4989853828c2
Ivan <ivanatora@gmail.com>
2017-03-09 16:56:03 +0200

    player moves and pans

--- public/js/custom.js
+++ public/js/custom.js
@@ -10,20 +10,24 @@ var mPlayer = L.marker([0, 0]).addTo(map);
 map.locate({setView: true, maxZoom: 16});
 
 
-function success(pos){
-    console.log('pos', pos)
-//    L.marker([pos.coords.latitude, pos.coords.longitude]).addTo(map)
-    mPlayer.setLatLng([pos.coords.latitude, pos.coords.longitude]);
+function fnMovePlayer(pos){
+    var oNewPosition = L.latLng(pos.coords.latitude, pos.coords.longitude);
+    var iDistance = map.getCenter().distanceTo(oNewPosition);
+    
+    if (iDistance > 300){
+        map.panTo(oNewPosition);
+    }
+    mPlayer.setLatLng(oNewPosition);
 }
 
-function error(err) {
+function fnMovePlayerError(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
 }
 
-var options = {
+var oMovePlayerOptions = {
   enableHighAccuracy: true,
   timeout: 5000,
   maximumAge: 0
 };
 
-navigator.geolocation.watchPosition(success, error, options);
+navigator.geolocation.watchPosition(fnMovePlayer, fnMovePlayerError, oMovePlayerOptions);
