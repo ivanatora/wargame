c66cbdc14845ec36ca0817888bf59e3db4dfc631
Ivan <ivanatora@gmail.com>
2017-03-09 10:00:12 +0200

    vendors

--- public/js/custom.js
+++ public/js/custom.js
@@ -6,3 +6,15 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 map.locate({setView: true, maxZoom: 16});
+
+function onLocationFound(e) {
+    console.log(e)
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
