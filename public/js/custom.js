3b8cd2d517a2037bc135b7d4c67fa0cf698ccc00
Ivan <ivanatora@gmail.com>
2017-03-09 16:01:35 +0200

    styles, navbars

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,7 +1,7 @@
 var map = L.map('map').fitWorld();
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
 }).addTo(map);
 
@@ -9,17 +9,6 @@ var mPlayer = L.marker([0, 0]).addTo(map);
 
 map.locate({setView: true, maxZoom: 16});
 
-//function onLocationFound(e) {
-//    console.log(e)
-//    var radius = e.accuracy / 2;
-//
-//    L.marker(e.latlng).addTo(map)
-//        .bindPopup("You are within " + radius + " meters from this point").openPopup();
-//
-//    L.circle(e.latlng, radius).addTo(map);
-//}
-//
-//map.on('locationfound', onLocationFound);
 
 function success(pos){
     console.log('pos', pos)
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -52,41 +52,7 @@
                             <li><a href="#">Storehouse</a></li>
                         </ul>
                     </li>
-
-<!--                    <li><a href="#">Dropdown <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li><a href="#">Something else here</a></li>
-                            <li class="divider"></li>
-                            <li class="dropdown-header">Nav header</li>
-                            <li><a href="#">Separated link</a></li>
-                            <li><a href="#">One more separated link <span class="caret"></span></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="#">Action</a></li>
-                                    <li><a href="#">Another action</a></li>
-                                    <li><a href="#">A long sub menu <span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="#">Action</a></li>
-                                            <li><a href="#">Something else here</a></li>
-                                            <li class="disabled"><a class="disabled" href="#">Disabled item</a></li>
-                                            <li><a href="#">One more link</a></li>
-                                            <li><a href="#">Menu item 1</a></li>
-                                            <li><a href="#">Menu item 2</a></li>
-                                            <li><a href="#">Menu item 3</a></li>
-                                            <li><a href="#">Menu item 4</a></li>
-                                            <li><a href="#">Menu item 5</a></li>
-                                            <li><a href="#">Menu item 6</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">Another link</a></li>
-                                    <li><a href="#">One more link</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>-->
                 </ul>
-
 
             </div><!--/.nav-collapse -->
         </div>
